690f70bda7418e836ccb93a695bdd30001849cb5
Kuku <manikchauhan029@gmail.com>
2025-07-19 01:31:25 +0530

    Fix syntax and import errors

---
+++ src/components/MarketOverview.js
@@ -0,0 +1,11 @@
+@@ .. @@
+                >
+                  {changeIcon} {Math.abs(crypto.price_change_percentage_24h).toFixed(2)}%
+                </motion.div>
+-                </div>
+-              </div>
++              </div>
+             </motion.li>
+           );
+         })}
+       </motion.ul>
---
+++ src/components/Scene3D.js
@@ -0,0 +1,6 @@
+@@ .. @@
+ import React, { useRef, useMemo } from 'react';
+ import { Canvas, useFrame } from '@react-three/fiber';
+-import { Sphere, Box, Torus, Text, OrbitControls, Stars } from '@react-three/drei';
+-import * as THREE from 'three';
++import { Box, Torus, Text, OrbitControls, Stars } from '@react-three/drei';
